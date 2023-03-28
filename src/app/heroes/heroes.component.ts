83b97f050a2e0af81524ada929694ca1519c4b70
Luidrocha <luidrocha@yahoo.com.br>
2023-03-28 16:08:28 -0300

    Adicionado Serviço HeroService

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,35 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
-import {HEROES} from '../mock-heroes';
+import { HeroService } from '../hero.service';
 
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.scss'],
 })
-
-export class HeroesComponent implements OnInit   {
-
+export class HeroesComponent implements OnInit {
   ngOnInit(): void {
-
     console.log('Heroes' + this.selectedHero);
   }
 
-heroes = HEROES;
+  heroes: Hero[] = [];
 
   selectedHero?: Hero;
 
-  onInit(){
+  constructor(private heroService: HeroService) {}
 
-  }
+  onInit() {}
 
-  onSelect(hero:Hero):void {
-
-    this.selectedHero=hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
     console.log(this.selectedHero);
   }
 
-
-
-};
-
+  getHeroes() {
+    this.heroes = this.heroService.getHeros();
+  }
+}
